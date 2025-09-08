1b0840a9f58949f5a54a717f0da3fc99e2db867f
tushar7789 <tusharjangid7@gmail.com>
2025-09-08 22:57:19 +0530

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import LandingPage from '@/components/LandingPage';
+import React from 'react';
+
 export default function Home() {
 
   return (
--- src/interfaces/interfaces.tsx
+++ src/interfaces/interfaces.tsx
@@ -56,19 +56,20 @@ export interface IrregularOptionInterface {
 export interface ButtonInterface {
     'text'?: string | undefined,
     'onClick'?: React.MouseEventHandler<HTMLDivElement> | undefined,
-    'style'?: ButtonStyleInterface | undefined
+    'style'?: ButtonStyleInterface
 }
 
 export interface ButtonStyleInterface {
-    'color'?: string | undefined,
-    'width'?: string | undefined,
-    'height'?: string | undefined,
-    'bgColor'?: string | undefined,
-    'border'?: string | undefined,
-    'borderRadius'?: string | undefined,
-    'paddingLeft'?: string | undefined,
-    'paddingRight'?: string | undefined,
-    'paddingTop'?: string | undefined,
-    'paddingBottom'?: string | undefined,
-    'cursor'?: string | undefined
+    'color'?: string,
+    'width'?: string,
+    'height'?: string,
+    'bgColor'?: string,
+    'border'?: string,
+    'borderRadius'?: string,
+    'paddingLeft'?: string,
+    'paddingRight'?: string,
+    'paddingTop'?: string,
+    'paddingBottom'?: string,
+    'cursor'?: string,
+    'fontSize'?: string
 }
